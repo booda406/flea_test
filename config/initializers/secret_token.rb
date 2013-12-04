7790d1b27643a541114ac48668d06d003fa8cdbe
Kelly <kelly@KellytekiMacBook-Pro.local>
2013-12-04 11:49:45 +0800

    init git

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FleaTest::Application.config.secret_key_base = '98aa9d10d86ba285bea0d461f59f31d2fa5b5894ac5f172d07921f000748b023f5c7ab29cc8897bbf0af76f827e1b99d0d940c407815a68a14f61d09c02bb8d7'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FleaTest::Application.config.session_store :cookie_store, key: '_flea_test_session'
